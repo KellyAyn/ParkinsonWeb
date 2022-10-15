096746183b01183380f5afc34941ccd866fdc53e
KellyAyn <vojtech.aug@gmail.com>
2022-10-15 13:56:59 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav>
+      <div class="nav-logo"></div>
+      <div class="nav-links">
+        <ul>
+          <li>
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about">About</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/contact">Contact</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/parkinsonsdisease">Parkinson's Disease</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
